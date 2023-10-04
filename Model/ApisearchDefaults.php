0b4e17b0eee43793eb2d226516c5f7bd79adce55
Marc <yuhu@mmoreram.com>
2023-10-04 14:30:04 +0200

    To version 2.1.3

--- Model/ApisearchDefaults.php
+++ Model/ApisearchDefaults.php
@@ -4,9 +4,9 @@ namespace Apisearch\Model;
 
 class ApisearchDefaults
 {
-    const DEFAULT_AS_ADMIN_URL = 'http://localhost:8300';
+    const DEFAULT_AS_ADMIN_URL = 'https://static.apisearch.cloud';
     const PLUGIN_NAME = 'apisearch';
-    const PLUGIN_VERSION = '2.1.2';
+    const PLUGIN_VERSION = '2.1.3';
     const DEFAULT_INDEX_PRODUCTS_WITHOUT_IMAGE = false;
     const DEFAULT_REAL_TIME_INDEXATION = false;
     const DEFAULT_AS_INDEX_PRODUCT_PURCHASE_COUNT = false;
